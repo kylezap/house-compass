e09085f251f2fbe6bef038f99b84d811efcdfd9f
kylezap <kzapcic+github@gmail.com>
2024-09-02 22:11:42 -0400

    design: add animation to directions results h2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {keyframes: {
+      slideUp: {
+        '0%': { transform: 'translateY(100%)', opacity: '0' },
+        '100%': { transform: 'translateY(0)', opacity: '1' },
+      },
+    },
+    animation: {
+      slideUp: 'slideUp 0.5s ease-out',
+    },},
   },
   plugins: [],
 }
